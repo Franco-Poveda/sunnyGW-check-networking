8a8ac6d7b0ef8da5645b920f124fb9e1cdde96cd
Franco-Poveda <franco.poveda@gmail.com>
2018-04-21 14:59:48 -0300

    added broker check

--- main.go
+++ main.go
@@ -5,14 +5,35 @@ import (
 	"html"
 	"strconv"
 
-	"github.com/raphamorim/go-rainbow"
-	"github.com/reiver/go-telnet"
+	rainbow "github.com/raphamorim/go-rainbow"
+	telnet "github.com/reiver/go-telnet"
 )
 
 func main() {
+	tunnel()
+	broker()
+}
+
+func tunnel() {
 	fmt.Printf("%s", rainbow.Bold(rainbow.Magenta("Conexion to tunnel server ")))
 
 	conn, err := telnet.DialTo("54.207.26.75:22")
+	if nil != err {
+		str := html.UnescapeString("&#" + strconv.Itoa(128683) + ";")
+		fmt.Println(str)
+		fmt.Println(err)
+	} else {
+		conn.Close()
+		str := html.UnescapeString("&#" + strconv.Itoa(9989) + ";")
+		fmt.Println(str)
+
+	}
+}
+
+func broker() {
+	fmt.Printf("%s", rainbow.Bold(rainbow.Magenta("Conexion to MQTT broker ")))
+
+	conn, err := telnet.DialTo("sunnyctl.povedaingenieria.com:1883")
 	if nil != err {
 		str := html.UnescapeString("&#" + strconv.Itoa(128683) + ";")
 		fmt.Println(str)
